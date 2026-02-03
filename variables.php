cc1b843ef0b2bf63c7a2aa4e090218b080936e07
dpage489 <dpage4@yc.edu>
2026-02-03 16:33:22 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Variables</title>
+</head>
+<body>
+
+<?php // Script 2.3 variable.php
+
+$phone = "9285552345";
+$name = "Daniel Page";
+$street = "Simple Street";
+$city = "Prescott";
+$state = "AZ";
+$zip = 16801;
+
+// Print the address:
+print "<p>The address is :<br>$street <br>$city $state $zip</p>";
+print "<p>My info is :<br>$name <br>$phone</p?>";
+
+?>
+</body>
+</html>

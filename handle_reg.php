55c84deac6d2b297b597fa6995de51fa0846c8cf
dpage489 <dpage4@yc.edu>
2026-02-15 20:58:20 -0700

    Add files via upload
    The PHP file for the register.html file

---
+++ handle_reg.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<title>Registration</title>
+</head>
+<body>
+<h1>Registration Results</h1>
+<?php // Script 6.2 - handle_reg.php
+/* This script recieves seven values from register.html:
+email, password, confirm, year, terms, color, submit */
+
+// Address error management, if you want.
+error_reporting(0);
+
+// Flag variable to track success:
+$okay = true;
+
+// If there were no errors, print a success message:
+if ($okay) {
+	print '<p>You have been successfully registered (but not really).</p>';
+}
+?>
+</body>
+</html>

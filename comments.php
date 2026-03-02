d79ce2a4639a08cb95330be3d9a39f4067ec539e
dpage489 <dpage4@yc.edu>
2026-03-01 19:49:30 -0700

    Add files via upload

---
+++ comments.php
@@ -0,0 +1,11 @@
+<?php
+// Single-line comment: this comment explains one line
+
+/*
+Multi-line comment:
+This comment can span multiple lines.
+Used for longer explanations.
+*/
+
+echo "PHP comments are not shown in the browser.";
+?>
